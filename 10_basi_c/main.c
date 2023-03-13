a7bb4547de616429f94d4198fa103481f5202bfe
Francesco Piazzi <francescopiazzi25@gmail.com>
2023-03-13 10:19:04 +0100

    aggiunte union

--- 10_basi_c/main.c
+++ 10_basi_c/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+union U{
+    int a;
+    float b;
+};
+
 int main(){
     int x=10;
     printf("variable x : %lu\n", sizeof x);
@@ -8,4 +13,11 @@ int main(){
     printf("int type : %lu\n", sizeof(short int));
     printf("expression 1.0/2.0 : %f\n", sizeof 1.0/2.0);
     printf("float: %lu\n", sizeof(float));
+
+    union U u1;
+    u1.a=4;
+    u1.b=5.6;
+
+    printf("union a: %d (un paciugo perche' ho assegnato b e a e b non possono coesistere)\n", u1.a);
+    printf("union b: %f\n", u1.b);
 }
