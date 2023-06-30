0d180a85697a2281b0f322d6ae04b65499faa8ef
Fre <francescopiazzi25@gmail.com>
2023-06-30 08:03:39 +0200

    hehe

--- 8_pipe/pipe_con_nome_out.c
+++ 8_pipe/pipe_con_nome_out.c
@@ -7,9 +7,9 @@
 
 
 int main (int argc, char *argv[]) {
-    int fd;   char * fifoName = "/tmp/fifo1";  
-    char str1[80], * str2 = "I'm a writer";
-    mkfifo(fifoName,S_IRUSR|S_IWUSR); //Create pipe if it doesn’t exist
+    int fd;   char *fifoName = "/tmp/fifo1";  
+    char str1[80], *str2 = "I'm a writer";
+    mkfifo(fifoName, S_IRUSR|S_IWUSR); //Create pipe if it doesn’t exist
     
     fd = open(fifoName, O_WRONLY); // Open FIFO for write only
     write(fd, str2, strlen(str2)+1); // write and close
