97997ca988ce8ab92c4383dc167a0f2ee9a68716
Fre <francescopiazzi25@gmail.com>
2023-06-30 15:17:38 +0200

    lel

--- 10_thread/thread_io.c
+++ 10_thread/thread_io.c
@@ -2,9 +2,9 @@
 
 #include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <unistd.h>
 
 
 struct ipair_t{
@@ -22,8 +22,8 @@ int main(void) {
     pthread_t t_id;
     ipair n;
     n.n1 = 10;
-    n.n2 = 31843;
+    n.n2 = 32620;
     pthread_create(&t_id, NULL, my_fun, (void *)&n);
-    printf("Executed thread with id %ld\n", t_id);
+    // printf("Executed thread with id %ld\n", t_id);
     sleep(3);
 }
